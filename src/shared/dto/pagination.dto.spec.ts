916930395e5aff5d949fb1ab221d8fd03fe7429c
Virginia Redondo <7237305+virginiar@users.noreply.github.com>
2026-04-07 14:37:43 +0200

    Añadiendo validación para la conversión de cadenas de texto en números en pagination.dto

--- src/shared/dto/pagination.dto.spec.ts
+++ src/shared/dto/pagination.dto.spec.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import { validate } from 'class-validator';
 import { PaginationDto } from './pagination.dto';
+import { plainToInstance } from 'class-transformer';
 
 describe('PaginationDto', () => {
   it('should validate with default values', async () => {
@@ -54,5 +55,16 @@ describe('PaginationDto', () => {
         expect(true).toBeFalsy();
       }
     });
+  });
+
+  it('should convert strings into numbers', async () => {
+    const input = { limit: '10', page: '2' };
+    const dto = plainToInstance(PaginationDto, input);
+
+    const errors = await validate(dto);
+
+    expect(errors.length).toBe(0);
+    expect(dto.limit).toBe(10);
+    expect(dto.page).toBe(2);
   });
 });
