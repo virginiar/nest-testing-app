300392f02b1a7d620ddf3338c04feef816236093
Virginia Redondo <7237305+virginiar@users.noreply.github.com>
2026-04-07 14:35:55 +0200

    Añadiendo pruebas de validación de límite y página en pagination.dto

--- src/shared/dto/pagination.dto.spec.ts
+++ src/shared/dto/pagination.dto.spec.ts
@@ -10,4 +10,49 @@ describe('PaginationDto', () => {
 
     expect(errors.length).toBe(0);
   });
+
+  it('should validate with valid data', async () => {
+    const dto = new PaginationDto();
+
+    dto.limit = 10;
+    dto.page = 1;
+
+    const errors = await validate(dto);
+
+    expect(errors.length).toBe(0);
+  });
+
+  it('should not validate with invalid page', async () => {
+    const dto = new PaginationDto();
+    dto.page = -1;
+
+    const errors = await validate(dto);
+
+    expect(errors.length).toBeGreaterThanOrEqual(1);
+
+    errors.forEach((error) => {
+      if (error.property === 'page') {
+        expect(error.constraints?.min).toBeDefined();
+      } else {
+        expect(true).toBeFalsy();
+      }
+    });
+  });
+
+  it('should not validate with invalid limit', async () => {
+    const dto = new PaginationDto();
+    dto.limit = -1;
+
+    const errors = await validate(dto);
+
+    expect(errors.length).toBeGreaterThanOrEqual(1);
+
+    errors.forEach((error) => {
+      if (error.property === 'limit') {
+        expect(error.constraints?.min).toBeDefined();
+      } else {
+        expect(true).toBeFalsy();
+      }
+    });
+  });
 });
