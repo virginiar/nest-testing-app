9e9e01ce3e19e4bb08338648a83b53201586069b
Virginia Redondo <7237305+virginiar@users.noreply.github.com>
2026-04-09 14:19:12 +0200

    Añadiendo pruebas de conexión entre PokemonController y PokemonService

--- src/pokemons/pokemons.controller.spec.ts
+++ src/pokemons/pokemons.controller.spec.ts
@@ -1,9 +1,35 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PokemonsController } from './pokemons.controller';
 import { PokemonsService } from './pokemons.service';
+import { Pokemon } from './entities/pokemon.entity';
+import { PaginationDto } from 'src/shared/dto/pagination.dto';
+
+const mockPokemons: Pokemon[] = [
+  {
+    id: 1,
+    name: 'bulbasaur',
+    type: 'grass',
+    hp: 45,
+    sprites: [
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png',
+    ],
+  },
+  {
+    id: 2,
+    name: 'ivysaur',
+    type: 'grass',
+    hp: 60,
+    sprites: [
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png',
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/2.png',
+    ],
+  },
+];
 
 describe('PokemonsController', () => {
   let controller: PokemonsController;
+  let service: PokemonsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -12,9 +38,36 @@ describe('PokemonsController', () => {
     }).compile();
 
     controller = module.get<PokemonsController>(PokemonsController);
+    service = module.get<PokemonsService>(PokemonsService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should have called the service with correct parameter', async () => {
+    const dto: PaginationDto = { limit: 10, page: 1 };
+
+    jest.spyOn(service, 'findAll');
+
+    await controller.findAll(dto);
+
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    expect(service.findAll).toHaveBeenCalled();
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    expect(service.findAll).toHaveBeenCalledWith(dto);
+  });
+
+  it('should have called the service and check the result', async () => {
+    const dto: PaginationDto = { limit: 10, page: 1 };
+
+    jest
+      .spyOn(service, 'findAll')
+      .mockImplementation(() => Promise.resolve(mockPokemons));
+
+    const pokemons = await controller.findAll(dto);
+
+    expect(pokemons).toBe(mockPokemons);
+    expect(pokemons.length).toBe(mockPokemons.length);
   });
 });
