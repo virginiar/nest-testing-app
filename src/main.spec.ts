dece3c992b88d5a28f7360d69dbd5066d5040b6d
Virginia Redondo <7237305+virginiar@users.noreply.github.com>
2026-04-09 14:29:55 +0200

    Añadiendo pruebas en Main

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { bootstrap } from './main';
+import { AppModule } from './app.module';
 
 jest.mock('@nestjs/core', () => ({
   NestFactory: {
@@ -30,5 +31,42 @@ describe('Main.ts Bootstrap', () => {
 
   it('should create application', async () => {
     await bootstrap();
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    expect(NestFactory.create).toHaveBeenCalledWith(AppModule);
+  });
+
+  it('should set global prefix', async () => {
+    await bootstrap();
+
+    expect(mockApp.setGlobalPrefix).toHaveBeenCalledWith('api');
+  });
+
+  it('should listen on port 3000 if env port not set', async () => {
+    await bootstrap();
+
+    expect(mockApp.listen).toHaveBeenCalledWith(3000);
+  });
+
+  it('should listen on env port', async () => {
+    process.env.PORT = '4200';
+
+    await bootstrap();
+
+    expect(mockApp.listen).toHaveBeenCalledWith(process.env.PORT);
+  });
+
+  it('should use global pipes', async () => {
+    await bootstrap();
+
+    expect(mockApp.useGlobalPipes).toHaveBeenCalledWith(
+      expect.objectContaining({
+        errorHttpStatusCode: 400,
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        validatorOptions: expect.objectContaining({
+          forbidNonWhitelisted: true,
+          whitelist: true,
+        }),
+      }),
+    );
   });
 });
